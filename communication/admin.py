96f413b0b5c1d6889f3fa1436f43d8e8cd272d49
idabblewith <109533655+idabblewith@users.noreply.github.com>
2023-04-21 12:36:53 +0800

    Added Comments Admin

--- communication/admin.py
+++ communication/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "category",
+        "payload",
+        "created_at",
+        "is_public",
+    ]
+
+    list_filter = [
+        "is_public",
+        "category",
+    ]
+
+    search_fields = [
+        "payload",
+        "user__username",
+    ]
